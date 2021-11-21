29bd26867bed1c1b01b97cba33f22e0cc5df3ffa
M.M <reach.wire@pm.me>
2021-11-22 01:08:08 +0200

    added input handler to form

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -4,18 +4,42 @@ export default class PostForm extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            title: ''
+        };
     }
 
     handleSubmit = event => {
         event.preventDefault();
+
+        const { title } = this.state;
+
+        const newPost = {
+            title, id: Date.now().toString()
+        }
+    }
+
+    changeInput = event => {
+        event.persist();
+
+        this.setState(prev => (
+            {...prev, ...{
+                [event.target.name]: event.target.value
+            }}
+        ))
     }
 
     render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <input type='text' className='form-control'/>
+                    <input
+                        type='text'
+                        className='form-control'
+                        value={this.state.title}
+                        name='title'
+                        onChange={this.changeInput}
+                    />
                     <button className='btn btn-success mt-2' type='submit'>Add post</button>
                 </form>
             </div>
