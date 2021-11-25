b9d8c72f555b86d068d0a4053ca159e40218ff64
M.M <reach.wire@pm.me>
2021-11-25 10:55:22 +0200

    fixed stop-words filtering - added convertation to lower case

--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -7,7 +7,8 @@ export function filterSpamWords({dispatch}) {
     return function (next) {
         return function (action) {
             if(action.type === CREATE_POST) {
-                const isSpamMessage = forbiddenWords.filter(w => action.payload.title.includes(w)).length;
+                const isSpamMessage = forbiddenWords.filter(w =>
+                    action.payload.title.toLowerCase().includes(w.toLowerCase())).length;
 
                 if(isSpamMessage) {
                     return dispatch(showAlert('Spam message detected. Please, check input.'));
